de2133cab9392e56480f83d3347fa83e01f2f1e2
Kadeem Austin <dev@kadeemaustin.ai>
2025-05-20 19:29:10 -0400

    fix: pass correct parameters when constructing model

--- prism/__main__.py
+++ prism/__main__.py
@@ -8,6 +8,7 @@ import datasets
 
 DATASET_NAME = "openwebtext"
 TEXT_COLUMN_NAME = "text"
+NUM_HEADS = 12
 BATCH_SIZE = 64
 CONTEXT_SIZE = 512
 VOCAB_SIZE = 16_384
@@ -17,14 +18,13 @@ EMBEDDING_SIZE = 256
 
 def main():
     try:
-        dataset = datasets.load_dataset(DATASET_NAME)
+        train_dataset = datasets.load_dataset(DATASET_NAME, split="train[:1%]")
     except Exception as e:
         raise TrainingError("An error occurred while loading the dataset", e) 
 
-    train_dataset = dataset['train']
-
     tokenizer = Tokenizer().train(train_dataset, TEXT_COLUMN_NAME, BATCH_SIZE, VOCAB_SIZE)
     model = Model(VOCAB_SIZE, EMBEDDING_SIZE, CONTEXT_SIZE)
+
     for epoch in range(NUM_EPOCHS):
         batches = to_batches(tokenizer, train_dataset, BATCH_SIZE, CONTEXT_SIZE)
         for batch in batches:
--- prism/model.py
+++ prism/model.py
@@ -12,12 +12,24 @@ class Model(nn.Module):
         context_size: int,
         num_heads: int = 12,
         d_key: int = 64,
-        d_value: int = 64
+        d_value: int = 64,
+        masked: bool = True,
+        dropout: float = 0.0
     ):
         super().__init__()
         self.embedding = Embedding(vocab_size, context_size, embedding_size)
         self.masked_multi_head_attention = MultiHeadAttention(
-            embedding_size, num_heads, d_key, d_value)
+            num_heads,
+            embedding_size,
+            d_key,
+            d_value,
+            masked=masked,
+            dropout=dropout
+        )
+        self.normalization = nn.LayerNorm(embedding_size)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.embedding(x)
+        x = self.embedding(x)
+        x = self.masked_multi_head_attention(x)
+        x = self.normalization(x)
+        return x
