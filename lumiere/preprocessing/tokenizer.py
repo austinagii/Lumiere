bf1fedd5717267b06e1eddff47242f186e1c1b77
Kadeem Austin <dev@kadeemaustin.ai>
2025-08-11 17:44:39 -0500

    feat: add tests for tokenizer

--- lumiere/preprocessing/tokenizer.py
+++ lumiere/preprocessing/tokenizer.py
@@ -28,56 +28,57 @@ class Tokenizer:
         self.tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel()
         self.tokenizer.decoder = decoders.ByteLevel()
         self.trainer = trainers.BpeTrainer(
-            vocab_size=vocab_size, min_frequency=min_frequency
+            vocab_size=vocab_size,
+            min_frequency=min_frequency,
+            show_progress=False,
+            special_tokens=[token.token for token in SPECIAL_TOKENS.values()],
         )
         self.tokenizer.add_special_tokens(
             [token.token for token in SPECIAL_TOKENS.values()]
         )
 
     def tokenize(self, text: str) -> list[str]:
-        """Tokenizes the specified text"""
+        """Returns a list of tokens for the specified text"""
         return self.tokenizer.encode(text).tokens
 
     def tokenize_all(
         self, corpus: Iterable[str], lazy: bool = False
     ) -> list[list[str]] | Generator[list[str], None, None]:
-        """Tokenizes the specified text in chunks of the given size
+        """Tokenizes the specified text
 
-        When `chunk_size` is not specified the encoding behavior becomes dynamic based
-        on the type of `text`. If text is a string type, then the entire str is encoded,
-        however, if text is a sequence of string types, then an iterator is returned
-        where containing the token ids of the encoded text sequence. Note that the
-        chunks do not cross sample boundaries and the chunks are not padded.
+        When `lazy` is `True`, a generator of lists of tokens is returned.
+        Otherwise, a list of lists of tokens is returned.
         """
         if lazy:
-            for text in corpus:
-                yield self.tokenize(text)
+            return (self.tokenize(text) for text in corpus)
         else:
             return [self.tokenize(text) for text in corpus]
 
-    def encode(self, tokens: list[str]) -> list[str]:
-        return self.tokenizer.encode(tokens, is_pretokenized=True).ids
+    def encode(self, tokens: list[str]) -> list[int]:
+        return [self.tokenizer.token_to_id(token) for token in tokens]
 
     def encode_all(
-        self, corpus: Sequence[str], lazy: bool = False
-    ) -> list[list[str]] | Generator[list[str], None, None]:
-        # if lazy:
-        #     for text in corpus:
-        #         yield self.tokenizer.encode(text, is_pretokenized=True).ids
-        # else:
-        #     return [
-        #         self.tokenizer.encode(text, is_pretokenized=True).ids for text in corpus
-        #     ]
-        return [
-            [self.tokenizer.token_to_id(token) for token in text] for text in corpus
-        ]
+        self, corpus: Sequence[list[str]], lazy: bool = False
+    ) -> list[list[int]] | Generator[list[int], None, None]:
+        if lazy:
+            return (
+                [self.tokenizer.token_to_id(token) for token in text] for text in corpus
+            )
+        else:
+            return [
+                [self.tokenizer.token_to_id(token) for token in text] for text in corpus
+            ]
 
-    def decode(self, token_ids: list[str]) -> str:
+    def decode(self, token_ids: list[int]) -> str:
         return self.tokenizer.decode(token_ids)
 
-    def decode_all(self, corpus: Sequence[list[str]]) -> Generator[str, None, None]:
-        for ids in corpus:
-            yield self.decode(ids)
+    def decode_all(
+        self, corpus: Sequence[list[int]], lazy: bool = False
+    ) -> list[str] | Generator[str, None, None]:
+        if lazy:
+            return (self.decode(ids) for ids in corpus)
+        else:
+            return [self.decode(ids) for ids in corpus]
 
     def train(self, corpus: Iterable[str]):
         self.tokenizer.train_from_iterator(corpus, self.trainer)
