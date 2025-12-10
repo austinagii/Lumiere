6b945f17a622d302972fb4316043eca9a5c26d44
Kadeem Austin <dev@kadeemaustin.ai>
2025-12-10 19:02:45 -0400

    refactor: introduce TrainingState record for persisting training state between epochs

--- lumiere/training/eval.py
+++ lumiere/training/eval.py
@@ -1,6 +1,5 @@
+from collections.abc import Iterable
 from dataclasses import dataclass
-from time import time
-from typing import Iterable
 
 import torch
 from deepscale.storage.clients.azure_blob_storage_client import (
@@ -15,11 +14,10 @@ from lumiere.models.transformer import Transformer
 
 
 @dataclass
-class EvaluationState:
+class EvalMetrics:
     avg_loss: float
     avg_perplexity: float
     num_batches: int
-    time_taken: float
 
 
 def evaluate(
@@ -27,7 +25,7 @@ def evaluate(
     data: Iterable[tuple[torch.Tensor, torch.Tensor]],
     device: torch.device = torch.device("cpu"),
     wandb_run: Run | None = None,
-) -> EvaluationState:
+) -> EvalMetrics:
     """Evaluate the transformer model on the specified data.
 
     Args:
@@ -39,7 +37,7 @@ def evaluate(
         wandb_run: Wandb run for logging. Set to None to disable logging (default: None)
 
     Returns:
-        EvaluationState: The evaluation state after processing all data.
+        EvalMetrics: The evaluation state after processing all data.
     """
     # Prepare the model for evaluation.
     model.to(device)
@@ -49,40 +47,35 @@ def evaluate(
     total_perplexity = 0.0
     num_batches = 0
 
-    start_time = time()
-    with torch.no_grad():
-        with tqdm(data, desc="Evaluating", leave=False) as pbar:
-            for x, padding_mask in pbar:
-                # Shift the input tokens to the left by one position to get the targets.
-                y = x[:, 1:].to(device)
-                # Shift x and its padding mask accordingly.
-                x = x[:, :-1].to(device)
-                padding_mask = padding_mask[:, :-1].to(device)
+    with torch.no_grad(), tqdm(data, desc="Evaluating", leave=False) as pbar:
+        for x, padding_mask in pbar:
+            # Shift the input tokens to the left by one position to get the targets.
+            y = x[:, 1:].to(device)
+            # Shift x and its padding mask accordingly.
+            x = x[:, :-1].to(device)
+            padding_mask = padding_mask[:, :-1].to(device)
 
-                # Process the batch and calculate the loss.
-                logits, _ = model(x, padding_mask)
-                batch_loss = F.cross_entropy(
-                    logits.view(-1, model.vocab_size),
-                    y.reshape(-1),
-                    ignore_index=SPECIAL_TOKENS["padding"].id,
-                )
-                batch_perplexity = torch.exp(batch_loss)
+            # Process the batch and calculate the loss.
+            logits, _ = model(x, padding_mask)
+            batch_loss = F.cross_entropy(
+                logits.view(-1, model.vocab_size),
+                y.reshape(-1),
+                ignore_index=SPECIAL_TOKENS["padding"].id,
+            )
+            batch_perplexity = torch.exp(batch_loss)
 
-                # Update the evaluation stats.
-                total_loss += batch_loss.item()
-                total_perplexity += batch_perplexity.item()
-                num_batches += 1
+            # Update the evaluation stats.
+            total_loss += batch_loss.item()
+            total_perplexity += batch_perplexity.item()
+            num_batches += 1
 
-                # Update the progress bar.
-                pbar.set_postfix(
-                    {
-                        "loss": f"{batch_loss:.4f}",
-                        "perplexity": f"{batch_perplexity:.4f}",
-                    }
-                )
-
-    end_time = time()
-    time_taken = end_time - start_time
+            # Update the progress bar.
+            pbar.set_postfix(
+                {
+                    "loss": f"{batch_loss:.4f}",
+                    "perplexity": f"{batch_perplexity:.4f}",
+                }
+            )
 
     # Log the evaluation results to wandb.
     if wandb_run is not None:
@@ -94,9 +87,8 @@ def evaluate(
                 }
             )
 
-    return EvaluationState(
+    return EvalMetrics(
         avg_loss=total_loss / num_batches,
         avg_perplexity=total_perplexity / num_batches,
         num_batches=num_batches,
-        time_taken=time_taken,
     )
