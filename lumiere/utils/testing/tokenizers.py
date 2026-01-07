f35e81b369fd9c479c674eac443ddc385827683c
Kadeem Austin <dev@kadeemaustin.ai>
2026-01-06 21:27:28 -0500

    feat: implement tokenization methods for ascii tokenizer

--- lumiere/utils/testing/tokenizers.py
+++ lumiere/utils/testing/tokenizers.py
@@ -3,14 +3,19 @@ from collections.abc import Generator, Iterable, Sequence
 from lumiere.data.preprocessing.tokenizer import Tokenizer
 
 
+MAX_ASCII_CODE = 127
+
+
 class AsciiTokenizer(Tokenizer):
     def tokenize(self, text: str, to_ids: bool = False) -> list[str] | list[int]:
-        pass
+        return [ord(char) if ord(char) < MAX_ASCII_CODE else -1 for char in text]
 
     def tokenize_all(
         self, corpus: Iterable[str], lazy: bool = False, to_ids: bool = False
-    ) -> list[list[str]] | Generator[list[str], None, None]:
-        pass
+    ) -> (
+        list[list[str]] | list[list[int]] | Generator[list[str] | list[int], None, None]
+    ):
+        return (self.tokenize(text, to_ids=to_ids) for text in corpus)
 
     def encode(self, tokens: list[str]) -> list[int]:
         pass
---
+++ tests/utils/testing/test_tokenizers.py
@@ -0,0 +1,33 @@
+import pytest
+
+from lumiere.utils.testing.tokenizers import AsciiTokenizer
+
+
+@pytest.fixture(scope="module")
+def tokenizer():
+    return AsciiTokenizer()
+
+
+class TestAsciiTokenizer:
+    def test_tokenize_returns_ascii_codes_for_each_character(self, tokenizer):
+        text = "Hello test!"
+        expected_output = [72, 101, 108, 108, 111, 32, 116, 101, 115, 116, 33]
+
+        assert tokenizer.tokenize(text) == expected_output
+
+    def test_tokenize_returns_sentinel_for_non_ascii_characters(self, tokenizer):
+        text = "Hello Ā!"
+        expected_output = [72, 101, 108, 108, 111, 32, -1, 33]
+
+        assert tokenizer.tokenize(text) == expected_output
+
+    def test_tokenize_all_returns_ascii_codes_for_tokens_in_all_sequences(
+        self, tokenizer
+    ):
+        corpus = ["Natsuki Subaru", "Emilia Tan"]
+        expected_output = [
+            [78, 97, 116, 115, 117, 107, 105, 32, 83, 117, 98, 97, 114, 117],
+            [69, 109, 105, 108, 105, 97, 32, 84, 97, 110],
+        ]
+
+        assert list(tokenizer.tokenize_all(corpus)) == expected_output
