54c8b912f9d8924240def54a86476c9ae9f75379
Kadeem Austin <dev@kadeemaustin.ai>
2026-02-09 18:47:56 -0400

    refactor: remove unused optimizer and scheduler loaders

--- lumiere/training/__init__.py
+++ lumiere/training/__init__.py
@@ -1,7 +1,5 @@
 from .config import Config
-from .optimizer_loader import OptimizerLoader
-from .scheduler_loader import SchedulerLoader
 from .trainer import Trainer
 
 
-__all__ = ["Trainer", "Config", "OptimizerLoader", "SchedulerLoader"]
+__all__ = ["Trainer", "Config"]
