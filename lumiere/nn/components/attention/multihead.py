fix: fix numerical instability in stable_softmax function

lumiere/nn/components/attention/multihead.py
@@ -220,4 +220,7 @@ def stable_softmax(x: torch.Tensor) -> torch.Tensor:
     Returns:
         Tensor of the same shape as the input tensor
     """
-    return torch.exp(x) / (torch.sum(torch.exp(x), dim=-1, keepdim=True) + 1e-9)
+    x_max = x.amax(dim=-1, keepdim=True)
+    x_max = torch.clamp(x_max, min=0)
+    exp_x = torch.exp(x - x_max)
+    return exp_x / (exp_x.sum(dim=-1, keepdim=True) + 1e-9)

lumiere/nn/components/blocks/standard.py
@@ -107,20 +107,22 @@ class StandardTransformerBlock(nn.Module):
 
         """
         # Compute the attention values and weights.
+        residual = x
         if hasattr(self, "normalization_1"):
             x = self.normalization_1(x)
 
         attention_values, attention_weights = self.attention(
             x, padding_mask=padding_mask
         )
-        x = x + self.dropout(attention_values)
+        x = residual + self.dropout(attention_values)
 
         # Compute the feed-forward output.
+        residual = x
         if hasattr(self, "normalization_2"):
             x = self.normalization_2(x)
 
         feedforward_out = self.feedforward(x)
-        x = x + self.dropout(feedforward_out)
+        x = residual + self.dropout(feedforward_out)
 
         if hasattr(self, "normalization_3"):
             x = self.normalization_3(x)